e48259609e9362e9c4c3c2fbb6ba26f88c58037c
Eugenebop <bopubop@gmail.com>
2026-05-27 00:43:23 +0400

    fix middleware client only

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
   if (!to.path.startsWith('/admin')) return
-
+  
+  if (import.meta.server) return
+  
   const cookie = useCookie('admin_auth')
   if (cookie.value !== 'true') {
     return navigateTo('/login')
